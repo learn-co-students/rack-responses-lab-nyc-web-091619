1fe32bf4355f4d4d8789f4edba86befb341db871
Connor Meth <connormeth27@gmail.com>
2019-10-07 12:47:57 -0400

    Done.

---
+++ app/application.rb
@@ -0,0 +1,18 @@
+class Application
+ 
+    def call(env)
+      resp = Rack::Response.new
+   
+      now = Time.now
+      
+   
+      if now.hour >= 12
+        resp.write "Afternoon"
+      else
+        resp.write "Morning"
+      end
+   
+      resp.finish
+    end
+   
+  end
